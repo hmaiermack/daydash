060d6f5fbab7dc6c3db781d13f3dd870726053bc
Mack Maier <hmaiermack@gmail.com>
2022-07-08 11:10:57 -0700

    init, working auth0

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,26 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import Auth0Provider from "next-auth/providers/auth0"
+
+// Prisma adapter for NextAuth, optional and can be removed
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "../../../server/db/client";
+
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    // ...add more providers here
+    Auth0Provider({
+      // for some reason env vars are throwing errors, clientId + secret should be string | undefined?
+      // @ts-ignore
+      clientId: process.env.AUTH0_CLIENT_ID,
+      // @ts-ignore
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      issuer: process.env.AUTH0_ISSUER,
+    })
+  ],
+};
+
+export default NextAuth(authOptions);
