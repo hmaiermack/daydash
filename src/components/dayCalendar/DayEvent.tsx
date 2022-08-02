be800f3f38e4e8f812c1e66baca2d97be4c3ec75
Mack Maier <hmaiermack@gmail.com>
2022-08-02 14:44:30 -0700

    grow based on total time of event/task

--- src/components/dayCalendar/DayEvent.tsx
+++ src/components/dayCalendar/DayEvent.tsx
@@ -1,6 +1,33 @@
 import { differenceInMinutes, startOfHour } from 'date-fns';
 import React from 'react'
 
+const getShiftPercentage = (startTime: Date): number => {
+    const minsInHour = 60
+    const timeSinceStartOfHour = differenceInMinutes(startTime, startOfHour(startTime))
+
+    const percentDifference = (timeSinceStartOfHour / minsInHour)
+
+
+
+    const initialTopOffset = 55
+    const shiftFullHour = 100
+
+    return ((shiftFullHour * percentDifference) + initialTopOffset)
+
+}
+
+const getHeight = (startTime: Date, endTime: Date): number => {
+    const defaultCellHeight = 5
+    const totalLengthInMins = differenceInMinutes(endTime, startTime)
+
+
+    const percentOfHour = totalLengthInMins / 60
+
+    console.log({totalLengthInMins, percentOfHour})
+
+    return defaultCellHeight * percentOfHour
+}
+
 export const DayEvent = ({taskId, taskTitle, taskStart, taskEnd, tagId, tagColorValue, tagName}: {
     taskId: string;
     taskTitle: string;
@@ -11,21 +38,11 @@ export const DayEvent = ({taskId, taskTitle, taskStart, taskEnd, tagId, tagColor
     tagName?: string | undefined;
 }) => {
 
-    const minsInHour = 60
-    const timeSinceStartOfHour = differenceInMinutes(taskStart, startOfHour(taskStart))
+    const shiftPercentage = getShiftPercentage(taskStart)
+    const divHeight = getHeight(taskStart, taskEnd)
 
-    const percentDifference = (timeSinceStartOfHour / minsInHour)
-
-
-
-    const initialTopOffset = 55
-    const shiftFullHour = 100
-
-    const shiftPercentage = (shiftFullHour * percentDifference) + initialTopOffset
-
-    console.log({percentDifference,shiftPercentage})
     return (
-        <div className='h-40 absolute mx-auto  w-40 bg-blue-300 flex flex-col justify-between items-center' style={{top: `${shiftPercentage}%`, transform: 'translate(12.5%)'}}>
+        <div className='absolute w-40 bg-blue-300 flex flex-col justify-between items-center' style={{top: `${shiftPercentage}%`, transform: 'translate(12.5%)', height: `${divHeight}rem`}}>
                 <div className='mt-2 font-medium text-gray-700'>{taskTitle}</div>
                 <div className='flex justify-between w-full p-2'>
                     <span className='text-white text-xs p-1 border rounded-sm'>Edit</span>
