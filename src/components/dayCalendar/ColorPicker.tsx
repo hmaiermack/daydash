21634c5a385df5120729139ad22158e5e439fd70
Mack Maier <hmaiermack@gmail.com>
2022-12-13 13:55:46 -0800

    mas colores

--- src/components/dayCalendar/ColorPicker.tsx
+++ src/components/dayCalendar/ColorPicker.tsx
@@ -3,7 +3,12 @@ import React, { useState, FocusEvent } from 'react'
 import { ControllerRenderProps } from 'react-hook-form'
 
 const ColorPicker = React.forwardRef<HTMLInputElement, (ControllerRenderProps & {disabled: boolean})>((props, ref) => {
-    const colors = ["#2196F3", "#6196F3","#2143F3","#2246F3","#67F87C","#2136F3",]
+    const colors = [
+        "#f06292", "#f44336",  "#9c27b0", "#673ab7",
+        "#3f51b5", "#2196f3", "#81d4fa", "#00bcd4",
+        "#009688", "#4caf50", "#8bc34a", "#cddc39",
+        "#ffeb3b", "#ffc107", "#ff9800", "#ff5722",
+     ]
     const [inputString, setInputString] = useState(props.value ? props.value : '')
     console.log('first')
   return (
@@ -32,10 +37,10 @@ const ColorPicker = React.forwardRef<HTMLInputElement, (ControllerRenderProps & 
                 }}
             />
             <Combobox.Button
-            className={`w-10 h-10  ml-2 rounded-full focus:outline-none focus:shadow-outline inline-flex p-2 shadow text-white`} style={{background: `${inputString}`}}>
+            className={`w-10 h-10  ml-2 rounded-full focus:outline-none focus:shadow-outline inline-flex p-2 shadow text-white ${open ? 'hidden' : ''}`} style={{background: `${inputString}`}}>
             <svg className={`w-6 h-6 fill-current`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M15.584 10.001L13.998 8.417 5.903 16.512 5.374 18.626 7.488 18.097z"/><path d="M4.03,15.758l-1,4c-0.086,0.341,0.015,0.701,0.263,0.949C3.482,20.896,3.738,21,4,21c0.081,0,0.162-0.01,0.242-0.03l4-1 c0.176-0.044,0.337-0.135,0.465-0.263l8.292-8.292l1.294,1.292l1.414-1.414l-1.294-1.292L21,7.414 c0.378-0.378,0.586-0.88,0.586-1.414S21.378,4.964,21,4.586L19.414,3c-0.756-0.756-2.072-0.756-2.828,0l-2.589,2.589l-1.298-1.296 l-1.414,1.414l1.298,1.296l-8.29,8.29C4.165,15.421,4.074,15.582,4.03,15.758z M5.903,16.512l8.095-8.095l1.586,1.584 l-8.096,8.096l-2.114,0.529L5.903,16.512z"/></svg>
             </Combobox.Button>
-            <Combobox.Options as="div" className="w-40 bg-white flex flex-wrap">
+            <Combobox.Options as="div" className="w-44 bg-white flex flex-wrap justify-between ml-4 p-1 rounded">
                 {inputString.length > 0 &&
                     <Combobox.Option value={inputString} as="div" className="hidden">
                     </Combobox.Option>
