97e8a72bddaf83fa1b8732246e7380da3e8098b8
Mack Maier <hmaiermack@gmail.com>
2022-12-16 16:13:14 -0800

    cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,35 +3,21 @@ import { useEffect, useState } from "react";
 import { signIn } from "next-auth/react";
 import useWindowSize from "../hooks/useWindowSize";
 import { Controller, useForm } from "react-hook-form";
-import Testing from "../components/dayCalendar/Testing";
-import ColorPicker from "../components/dayCalendar/ColorPicker";
 import { CreateModalProvider } from "../context/modalContext";
-import EventInteractionModal from "../components/dayCalendar/EventInteractionModal";
 import { EventInteractionModalProvider } from "../context/EventInteractionModalContext";
+import CalendarContainer from "../components/dayCalendar/CalendarContainer";
 
 
 
 
 const Home: NextPage = () => {
-  const [date, setDate] = useState(new Date)
-  // const getTasks = trpc.useQuery(["tasks.tasks", {date}]);
-  const { register, handleSubmit, watch, formState: { errors }, control } = useForm();
-
-  const {width} = useWindowSize()
-
-
-
   return (
     <CreateModalProvider>
       <EventInteractionModalProvider>
 
         <div>
-        {/* <ColorPicker name="test" control={control}/> */}
           <button onClick={()=> signIn()} className="mx-4">Sign in</button>
-        {/* {width && 
-          <Table screenWidth={width}/>
-        } */}
-        <Testing />
+        <CalendarContainer />
 
         <div className="w-full h-40 bg-green-400 mt-5"></div>
         </div>
