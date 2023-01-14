aab8649d034b2ed1a6adf12617fe5788c799b73b
Mack Maier <hmaiermack@gmail.com>
2023-01-14 08:20:53 -0800

    form input auth and signin

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -6,6 +6,7 @@ import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import Link from 'next/link'
 import FormLabel from '../../components/general/form/FormLabel'
+import FormInput from '../../components/general/form/FormInput'
 
 type Inputs = {
     email: string
@@ -61,11 +62,9 @@ const register = () => {
             <fieldset>
                 <legend className='text-center text-2xl font-semibold mb-4'>Create new account</legend>
                 <FormLabel htmlFor="email">Email</FormLabel>
-                <input type="email" className={`appearance-none block bg-white text-gray-900 border font-medium ${errors.email ? 'border-red-500 ring-1 ring-red-500' : 'border-gray-400 focus:outline-none'} rounded-lg py-3 px-3 leading-tight`} {...register("email")}/>
-                    {errors.email && <p className="text-red-500 text-xs italic mt-1">{errors.email.message}</p>}
+                <FormInput register={register} error={errors.email} label='email' type='email' />
                 <FormLabel htmlFor="password" >Password</FormLabel>
-                <input type="password" className={`appearance-none block bg-white text-gray-900 border font-medium ${errors.password ? 'border-red-500 ring-1 ring-red-500' : 'border-gray-400 focus:outline-none'} rounded-lg py-3 px-3 leading-tight`} {...register("password")}/>
-                    {errors.password && <p className="text-red-500 text-xs italic mt-1">{errors.password.message}</p>}
+                <FormInput register={register} error={errors.password} label='password' type='password' />
             </fieldset>
             <button type="submit" className='mx-auto p-4 bg-green-400 text-white hover:bg-green-500 rounded-lg mt-4'>Register</button>
         </form>
--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -6,6 +6,7 @@ import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import Link from 'next/link'
 import FormLabel from '../../components/general/form/FormLabel'
+import FormInput from '../../components/general/form/FormInput'
 
 type Inputs = {
     email: string
@@ -25,6 +26,7 @@ const signin = () => {
 
     const router = useRouter()
     const onSubmit = async (data: Inputs) => {
+        console.log(data)
         setErr(undefined)
         signIn('credentials', {
             redirect: false,
@@ -49,11 +51,9 @@ const signin = () => {
                 <legend className='text-center text-2xl font-semibold mb-4'>Sign In</legend>
                 {err && <p className='text-red-500 text-xs italic mb-2'>{err}</p>}
                 <FormLabel htmlFor="email" >Email</FormLabel>
-                <input type="email" className={`appearance-none block bg-white text-gray-900 border font-medium ${errors.email ? 'border-red-500 ring-1 ring-red-500' : 'border-gray-400 focus:outline-none'} rounded-lg py-3 px-3 leading-tight`} {...register("email")}/>
-                    {errors.email && <p className="text-red-500 text-xs italic mt-1">{errors.email.message}</p>}
+                <FormInput register={register} error={errors.email} label='email' type='email'/>
                 <FormLabel htmlFor="password">Password</FormLabel>
-                <input type="password" className={`appearance-none block bg-white text-gray-900 border font-medium ${errors.password ? 'border-red-500 ring-1 ring-red-500' : 'border-gray-400 focus:outline-none'} rounded-lg py-3 px-3 leading-tight`} {...register("password")}/>
-                    {errors.password && <p className="text-red-500 text-xs italic mt-1">{errors.password.message}</p>}
+                <FormInput register={register} error={errors.password} label='password' type='password'/>
             </fieldset>
             <button type="submit" className='mx-auto p-4 bg-green-400 text-white hover:bg-green-500 rounded-lg mt-4'>Sign In</button>
         </form>
