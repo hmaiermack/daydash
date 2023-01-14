7ae4c69e6837ee75ed20216eaa4f4a067fea14a6
Mack Maier <hmaiermack@gmail.com>
2023-01-14 06:57:04 -0800

    runtime validation for registration

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -1,26 +1,38 @@
 import { prisma } from "../../../server/db/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import { hash } from "bcrypt";
+import { z } from "zod";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { email, password } = req.body;
-  const exists = await prisma.user.findUnique({
-    where: {
-      email,
-    },
+  const schema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
   });
-  if (exists) {
-    res.status(400).send("User already exists");
-  } else {
-    const user = await prisma.user.create({
-      data: {
+
+  try {
+    schema.parse(req.body);
+    const { email, password } = req.body;
+    const exists = await prisma.user.findUnique({
+      where: {
         email,
-        password: await hash(password, 10),
       },
     });
-    res.status(200).json(user);
+    if (exists) {
+      res.status(400).send("User already exists");
+    } else {
+      const user = await prisma.user.create({
+        data: {
+          email,
+          password: await hash(password, 10),
+        },
+      });
+      res.status(200).json(user);
+    }
+    
+  } catch (error) {
+    res.status(400).send(error);
   }
 }
